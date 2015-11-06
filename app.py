ffadc2e2e920cf3262c0956b0ca6fbb48bfe38a6
Justin Pacquing <pacqqu@gmail.com>
2015-11-06 13:54:50 -0500

    more initial things

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 import soundcloud
 
+from flask import Flask, redirect, render_template, request, session
+
+app = Flask(__name__)
+
 @app.route("/", methods=['GET'])
 def home():
     return render_template("home.html")
+
+if __name__ == '__main__':
+    app.dbug = True
+    app.run(host='0.0.0.0',port=8000)
