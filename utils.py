a6c5e456f227a57566f8876f5d1073870f4148c5
Gredoz <stuyredoz@gmail.com>
2015-11-15 20:09:42 -0500

    picUser is giving trouble

--- utils.py
+++ utils.py
@@ -18,8 +18,6 @@ def getWidget(track):
     embed_info = sclient.get('/oembed', url=trackurl)
     return embed_info.html
 
-#def findPic(input):
-
 
 #*********************Flickrapi******************************
 
@@ -28,21 +26,41 @@ api_secret = u'89c00afdacfc0a80'
     
 flickr = flickrapi.FlickrAPI(api_key, api_secret,format='parsed-json')
 
+#With inputted search query, return image url of top result from flickr
 def getPic(search):
     photoset = flickr.photos.search(text=search, per_page='1')
-   # sets = flickr.photosets.getList(text=search)
-   # imgurl = "https://farm" + {farm-id} + ".staticflickr.com/" + {server-id} + "/" + {id} + "_" + {secret} + ".jpg"
-    print photoset
-"""
-------------------------------JUSTIN---------------------------------------------------
-Currently, this works, but it just returns the JSON format of the one picture that we need to display
-The link to the picture (if using iframe) can be found at:
- "https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg"
-With all the things in {} being found in the JSON.
-I'm certain that the link works, as I've tried doing it manually, but I dont understand how to extract
-the information I need from the JSON that it returns via the computer.
+    #Photoset is Dictionary, with sub-dict 'photos' representing all results, containing key 'photo' whose value is photos according to search query in 'photo',
+    #key 'photo' contain list of sub-dicts that represent search-result photos
+    #print photoset['photos']['photo']
+    p = photoset['photos']['photo'][0]
+    #print p
+    farmid = str(p['farm'])
+    servid = str(p['server'])
+    id = str(p['id'])
+    sec = str(p['secret'])
+    imgurl = "https://farm" + farmid + ".staticflickr.com/" + servid + "/" + id + "_" + sec + ".jpg"
+    return imgurl
 
-"""
+
+#Returns the Title of the picture
+def picTitle(search):
+    photoset = flickr.photos.search(text = search, per_page='1')
+    p = photoset['photos']['photo'][0] 
+    title = p['title']
+    return title
+
+
+#Returns the Username of the person who uploaded the picture
+def picUser(search):
+    photoset = flickr.photos.search(text = search, per_page='1')
+    p = photoset['photos']['photo'][0]
+    print p
+    ownerid= str(p['owner'])
+    #print ownerid
+    j = flickr.people.getInfo(ownerid) '<==error is here'
+    '^ random variable cause I thought that it was the cause of an error'
+    #print "anything"
+    print user[username]
 
 
 #flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json')
@@ -51,9 +69,9 @@ the information I need from the JSON that it returns via the computer.
 
 #print('First set title: %s' % title)
 
-print(getPic("greg"))
-
-
+getPic("Greg")
+picTitle("Greg")
+#picUser("Greg")
 
 #track = findTrack("Angels")
 # print getWidget(track) 
